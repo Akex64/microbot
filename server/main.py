fdd9379af9a91c6864b59901301ac54e7ae700e3
Akex64 <61739633+Akex64@users.noreply.github.com>
2021-01-31 22:42:23 +0000

    Added basic server concept
    Added base of server

---
+++ server/main.py
@@ -0,0 +1,31 @@
+from microbit import *
+
+bots = [0]
+bot_states = [0]
+
+def on_forever():
+    def on_received_string(receivedString):
+        if "connect-" in receivedString:
+            bot_id = int(receivedString.split('connect-')[1])
+            bots.push(bot_id)
+            bot_states[bot_id] = 3 # connecting
+            print(f'[microbot] new connection ({str(bot_id)})')
+
+        if "pong-" in receivedString:
+            bot_id = int(receivedString.split('pong-')[1])
+            if bot_states[bot_id] == 4:
+                bot_states[bot_id] = 2 # alive
+
+    for bot_iterator in range(1, len(bots)):
+        bot_id = bots[bot_iterator]
+        radio.send_string(f'ping-{str(bot_id)}')
+        bot_states[bot_id] = 4 # waiting for response
+
+    radio.on_received_string(on_received_string)
+
+    basic.show_number(len(bots)-1)
+
+radio.set_group(231) # running on group 231 / port 231
+radio.set_transmit_power(7) # set high transmit power.
+radio.set_transmit_serial_number(False) # don't leak serial number to bots :/
+basic.forever(on_forever) # create thread
