bd93a85c0a4236ae2b116777d22c9a90f5c71d5f
Akex64 <61739633+Akex64@users.noreply.github.com>
2021-01-31 23:26:39 +0000

    Added kill features
    Kill and print to bots

--- server/main.py
+++ server/main.py
@@ -16,16 +16,29 @@ def on_forever():
             if bot_states[bot_id] == 4:
                 bot_states[bot_id] = 2 # alive
 
+    def kill_bots():
+        for bot_iterator in range(1, len(bots)):
+            bot_id = bots[bot_iterator]
+            radio.send_string(f'kill-{str(bot_id)}')
+            bot_states[bot_id] = 4 # waiting for response
+    def print_pwn():
+        for bot_iterator in range(1, len(bots)):
+            bot_id = bots[bot_iterator]
+            radio.send_string(f'pwn-{str(bot_id)}')
+            bot_states[bot_id] = 4 # waiting for response
     for bot_iterator in range(1, len(bots)):
         bot_id = bots[bot_iterator]
         radio.send_string(f'ping-{str(bot_id)}')
         bot_states[bot_id] = 4 # waiting for response
 
     radio.on_received_string(on_received_string)
+    input.on_button_pressed(Button.A, kill_bots)
+    input.on_button_pressed(Button.B, print_pwn)
 
     basic.show_number(len(bots)-1)
 
 radio.set_group(231) # running on group 231 / port 231
 radio.set_transmit_power(7) # set high transmit power.
 radio.set_transmit_serial_number(False) # don't leak serial number to bots :/
+print('[microbot] initialized radio')
 basic.forever(on_forever) # create thread
